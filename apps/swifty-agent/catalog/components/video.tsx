4a26a8548d3a210c8287d7788f6fb2f5c0649990
tianchenghang <161043261@qq.com>
2026-08-17 23:51:26 +0800

    feat: Introduce new features

--- apps/swifty-agent/catalog/components/video.tsx
+++
@@ -1,6 +0,0 @@
-import { createComponentImplementation } from "@a2ui/react/v0_9";
-import { VideoApi } from "@a2ui/web_core/v0_9/basic_catalog";
-
-export const Video = createComponentImplementation(VideoApi, ({ props }) => {
-  return <video src={props.url} controls className="w-full rounded-lg" />;
-});
